ffbc96c18751ecacb623e27eb44e286169e42104
Vespertino <Vespertino@C04PC17.informatica.comercio>
2023-02-27 19:42:57 +0100

    Get Marca

--- src/GuillermoDeCarlosMuñozMotos.java
+++ src/GuillermoDeCarlosMuñozMotos.java
@@ -1,4 +1,17 @@
 
 public class GuillermoDeCarlosMuñozMotos {
-
+	public String marca;
+	public String modelo;
+	public int cv;
+	
+	public GuillermoDeCarlosMuñozMotos(String marca, String modelo, int cv) {
+		this.marca=marca;
+		this.modelo=modelo;
+		this.cv=cv;
+	}
+	
+	 public void setMarca(String marca){
+             this.marca = marca;
+     }
+	 
 }
